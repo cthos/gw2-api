22e212df5ffd3bd9f254f68e76474311923927b3
cthos <daginus@gmail.com>
2016-01-12 17:39:58 -0800

    Adds the skills endpoint and a helper method to get profession skills.

--- test/api.test.js
+++ test/api.test.js
@@ -15,16 +15,16 @@ describe('GW2API', function () {
       });
     });
   });
-  
+
   describe('Cache', function () {
     before(function () {
       api.setCache(true);
     });
-    
+
     after(function () {
       api.setCache(false);
     });
-    
+
     it ('Should store continents in cache', function () {
       return api.getContinents().then(function(res) {
         var continents = api.getStorage().getItem(md5('/continents'));
@@ -32,13 +32,13 @@ describe('GW2API', function () {
         assert.equal(continents.length, 2);
         assert.deepEqual(res, continents);
       }).then(function (res) {
-        
+
       });
     });
-    
+
     it ('Should return different results on single item calls', function () {
       var itemResult1;
-      
+
       return api.getItems(15).then(function (res) {
         itemResult1 = res;
         return api.getItems(411);
@@ -46,10 +46,10 @@ describe('GW2API', function () {
         assert.notEqual(itemResult1.name, res.name);
       });
     });
-    
+
      it ('Should return different results on multiple item calls', function () {
       var itemResult1;
-      
+
       return api.getItems([15, 411]).then(function (res) {
         itemResult1 = res;
         return api.getItems([15, 211]);
@@ -57,10 +57,10 @@ describe('GW2API', function () {
         assert.notDeepEqual(itemResult1, res);
       });
     });
-    
+
     it ('Should return the same result on different id orders', function () {
       var itemResult1;
-      
+
       return api.getItems([15, 411]).then(function (res) {
         itemResult1 = res;
         return api.getItems([411, 15]);
@@ -69,20 +69,20 @@ describe('GW2API', function () {
       });
     });
   });
-  
+
   describe('Currencies', function () {
     it('Should return a list of currencies', function () {
       return api.getCurrencies().then(function (res) {
         assert.equal(res.length > 0, true);
       });
     });
-    
+
     it('Should Return a single currency', function () {
       return api.getCurrencies(1).then(function (currency) {
         assert.equal(currency.name, 'Coin');
-      }); 
+      });
     });
-    
+
     it('Should Return multiple currencies', function () {
       return api.getCurrencies([1, 2]).then(function (res) {
         res.forEach(function (item) {
@@ -92,24 +92,24 @@ describe('GW2API', function () {
             assert.equal(item.name, 'Karma');
           }
         });
-      }); 
+      });
     });
   });
-  
+
   describe('Items', function () {
     it ('Should have items', function () {
       return api.getItems().then(function (res) {
         assert.equal(res.length > 0, true);
       });
     });
-    
+
     it('Should get a single item', function () {
       return api.getItems(15).then(function (res) {
         assert.equal(Array.isArray(res), false);
         assert.equal(res.name, "Abomination Hammer");
       });
     });
-    
+
     it('Should get multiple items', function () {
       return api.getItems([15, 411]).then(function (res) {
         assert.equal(Array.isArray(res), true);
@@ -117,21 +117,21 @@ describe('GW2API', function () {
       });
     });
   });
-  
+
   describe('Materials', function () {
     it ('Should have materials', function () {
       return api.getMaterials().then(function (res) {
         assert.equal(res.length > 0, true);
       });
     });
-    
+
     it('Should get a single material', function () {
       return api.getMaterials(5).then(function (res) {
         assert.equal(Array.isArray(res), false);
         assert.equal(res.name, "Cooking Materials");
       });
     });
-    
+
     it('Should get multiple materials', function () {
       return api.getMaterials([5, 6]).then(function (res) {
         assert.equal(Array.isArray(res), true);
@@ -140,21 +140,21 @@ describe('GW2API', function () {
       });
     });
   });
-  
+
   describe('Recipes', function () {
     it ('Should have recipes', function () {
       return api.getRecipes().then(function (res) {
         assert.equal(res.length > 0, true);
       });
     });
-    
+
     it('Should get a single recipe', function () {
       return api.getRecipes(7319).then(function (res) {
         assert.equal(Array.isArray(res), false);
         assert.equal(res.output_item_id, 46742);
       });
     });
-    
+
     it('Should get multiple recipes', function () {
       return api.getRecipes([1, 2]).then(function (res) {
         assert.equal(Array.isArray(res), true);
@@ -162,25 +162,54 @@ describe('GW2API', function () {
       });
     });
   });
-  
+
   describe('Achievements', function () {
     it('Should get achievements', function () {
       return api.getAchievements().then(function (res) {
         assert.equal(res.length > 0, true);
       });
     });
-    
+
     it('Should get a single achievement', function () {
       return api.getAchievements(1344).then(function (res) {
         assert.equal(res.name, "Live on the Edge");
       });
     });
-    
+
     it('Should return daily achievements', function () {
       return api.getDailyAchievements().then(function (res) {
         assert.equal(res.pve.length > 0, true);
         assert.equal(typeof res.pve[0], 'object');
       });
+    });
+  });
+
+  describe('Skills', function () {
+    it('Should get skills', function () {
+      return api.getSkills().then(function (res) {
+        assert.equal(res.length > 0, true);
+      });
+    });
+
+    it('Should get a single skill', function () {
+      return api.getSkills(30488).then(function (skill) {
+        assert.equal(skill.name, '"Your Soul Is Mine!"')
+      });
+    });
+
+    it('Should get multiple skills', function () {
+      return api.getSkills([30488, 10527]).then(function (skills) {
+        assert.equal(Array.isArray(skills), true);
+        assert.equal(skills.length, 2);
+      });
+    });
+
+    it('Should get profession skills', function () {
+      return api.getProfessionSkills('Necromancer').then(function (skills) {
+        skills.forEach(function (skill) {
+          assert.equal(skill.professions.indexOf('Necromancer') != -1, true);
+        });
+      })
     });
   });
 });
